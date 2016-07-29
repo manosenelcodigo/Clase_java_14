b2eb2c76636f7030cc653484ddf8878b44d776e9
#manosenelcódigo <peligrosamente@gmail.com>
2016-07-28 22:02:25 -0400

    Create Panel3.java

---
+++ graficas/Panel3.java
@@ -0,0 +1,18 @@
+
+package graficas;
+
+
+import java.awt.FlowLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class Panel3 extends JPanel 
+{
+    public Panel3()
+    {
+        setLayout(new FlowLayout());
+        add(new JLabel("Todos los derechos están reservados a #manosenelcódigo"));
+    }
+}
